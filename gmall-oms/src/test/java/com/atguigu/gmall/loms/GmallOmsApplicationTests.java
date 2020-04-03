9a90077bd520484b0655282dc87041b2cdb84c17
halolzh <koyy821@qq.com>
2020-04-04 02:30:49 +0800

    项目结构创建完成

---
+++ gmall-oms/src/test/java/com/atguigu/gmall/loms/GmallOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.loms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
